4db71cf7a09fa7902d1d7f542cd6118dd90f5374
Axel Piras <pirasaxel@gmail.com>
2021-08-26 17:46:17 +0200

    changements MessageModel

--- models/messages.js
+++ models/messages.js
@@ -7,10 +7,6 @@ const messageSchema = mongoose.Schema({
   date: Date,
 });
 
-<<<<<<< HEAD
 const MessageModel = mongoose.model("messages", messageSchema);
-=======
-const MessagesModel = mongoose.model('messages', message)
->>>>>>> ae864fdc99cee76e7d98b59ab63aade19adf3465
 
 module.exports = MessageModel;
