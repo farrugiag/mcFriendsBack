a9bfd7046e90e47c34ae37b465d051316fae0eda
Maxime Violi <maxime.violi@gmail.com>
2021-08-26 17:30:38 +0200

    ajout model messages

---
+++ models/messages.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const messageSchema = mongoose.Schema({
+    emetteur: {type: mongoose.Schema.Types.Objectif, ref: 'users'},
+    recepteur: {type: mongoose.Schema.Types.Objectif, ref: 'users'},
+    message: String,
+    date: Date
+})
+
+const MessagesModel = mongoose.model('messages', message);
+
+module.exports = MessagesModel
