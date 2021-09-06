e96c8dd36d9835d0de330a749afeb8d378a54af0
Maxime Violi <maxime.violi@gmail.com>
2021-09-06 12:45:54 +0200

    ajout route feed-profil

--- routes/index.js
+++ routes/index.js
@@ -27,14 +27,14 @@ router.get("/", function (req, res, next) {
 
 //POST SIGNUP PARTICULIER
 router.post("/signup-particulier", async function (req, res, next) {
-  console.time("TIME SIGNUP PARTICULIER");
-  console.log("req.body PARTICULIERS", req.body);
-  console.log("centres d'interet", req.body.centresDinteret);
-  console.log("quartiers fav", req.body.quartiersFavoris);
-  console.time("TIME SIGNUP PARTICULIER USERMODEL BDD");
+  // console.time("TIME SIGNUP PARTICULIER");
+  // console.log("req.body PARTICULIERS", req.body);
+  // console.log("centres d'interet", req.body.centresDinteret);
+  // console.log("quartiers fav", req.body.quartiersFavoris);
+  // console.time("TIME SIGNUP PARTICULIER USERMODEL BDD");
   const findIfUserExists = await UserModel.findOne({ email: req.body.email });
-  console.timeEnd("TIME SIGNUP PARTICULIER USERMODEL BDD");
-  console.log("findIfUserExists", findIfUserExists);
+  // console.timeEnd("TIME SIGNUP PARTICULIER USERMODEL BDD");
+  // console.log("findIfUserExists", findIfUserExists);
   if (findIfUserExists) {
     //ON VÉRIFIE QUE SI L'EMAIL EST DÉJÀ UTILISÉ, ON NE CRÉE PAS UN DEUXIÈME COMPTE DANS LA BASE DE DONNÉES
     res.json({
@@ -44,13 +44,13 @@ router.post("/signup-particulier", async function (req, res, next) {
   }
   const cost = 18;
   const hash = bcrypt.hashSync(req.body.password, cost);
-  console.time("TIME SIGNUP PARTICULIER QUARTIERMODEL BDD");
+  // console.time("TIME SIGNUP PARTICULIER QUARTIERMODEL BDD");
   const quartierActivity = await QuartierModel.findOne({
     name: req.body.quartierActivity,
   });
-  console.timeEnd("TIME SIGNUP PARTICULIER QUARTIERMODEL BDD");
+  // console.timeEnd("TIME SIGNUP PARTICULIER QUARTIERMODEL BDD");
   const quartierId = quartierActivity._id;
-  console.time("TIME SIGNUP PARTICULIER USERMODEL SAVE BDD");
+  // console.time("TIME SIGNUP PARTICULIER USERMODEL SAVE BDD");
   const newUser = new UserModel({
     status: "Particulier",
     prenom: req.body.prenom,
@@ -65,21 +65,21 @@ router.post("/signup-particulier", async function (req, res, next) {
     token: uid2(32),
     password: hash,
   });
-  console.time("TIME SIGNUP PARTICULIER USERMODEL SAVE BDD");
+  // console.time("TIME SIGNUP PARTICULIER USERMODEL SAVE BDD");
   const newUserSaved = await newUser.save();
-  console.timeEnd("TIME SIGNUP PARTICULIER USERMODEL SAVE BDD");
-  console.timeEnd("TIME SIGNUP PARTICULIER");
+  // console.timeEnd("TIME SIGNUP PARTICULIER USERMODEL SAVE BDD");
+  // console.timeEnd("TIME SIGNUP PARTICULIER");
   res.json({ result: true, token: newUserSaved.token });
 });
 
 // POST SIGNUP COMMERCANT
 router.post("/signup-commercant", async function (req, res, next) {
-  console.time("TIME SIGNUP COMMERCANT");
-  console.log("req.body COMMERCANTS", req.body);
-  console.time("TIME SIGNUP COMMERCANT USERMODEL FINDONE");
+  // console.time("TIME SIGNUP COMMERCANT");
+  // console.log("req.body COMMERCANTS", req.body);
+  // console.time("TIME SIGNUP COMMERCANT USERMODEL FINDONE");
   const findIfUserExists = await UserModel.findOne({ email: req.body.email });
-  console.timeEnd("TIME SIGNUP COMMERCANT USERMODEL FINDONE");
-  console.log("findIfUserExists", findIfUserExists);
+  // console.timeEnd("TIME SIGNUP COMMERCANT USERMODEL FINDONE");
+  // console.log("findIfUserExists", findIfUserExists);
   if (findIfUserExists) {
     //ON VÉRIFIE QUE SI L'EMAIL EST DÉJÀ UTILISÉ, ON NE CRÉE PAS UN DEUXIÈME COMPTE DANS LA BASE DE DONNÉES
     res.json({
@@ -89,11 +89,11 @@ router.post("/signup-commercant", async function (req, res, next) {
   }
   const cost = 18;
   const hash = bcrypt.hashSync(req.body.password, cost);
-  console.time("TIME SIGNUP COMMERCANT QUARTIERMODEL FINDONE");
+  // console.time("TIME SIGNUP COMMERCANT QUARTIERMODEL FINDONE");
   const quartierActivity = await QuartierModel.findOne({
     name: req.body.quartierActivity,
   });
-  console.timeEnd("TIME SIGNUP COMMERCANT QUARTIERMODEL FINDONE");
+  // console.timeEnd("TIME SIGNUP COMMERCANT QUARTIERMODEL FINDONE");
   const quartierId = quartierActivity._id;
   const newUser = new UserModel({
     status: "Commercant",
@@ -106,17 +106,17 @@ router.post("/signup-commercant", async function (req, res, next) {
     token: uid2(32),
     password: hash,
   });
-  console.time("TIME SIGNUP COMMERCANT USERMODEL SAVE");
+  // console.time("TIME SIGNUP COMMERCANT USERMODEL SAVE");
   const newUserSaved = await newUser.save();
-  console.timeEnd("TIME SIGNUP COMMERCANT USERMODEL SAVE");
-  console.timeEnd("TIME SIGNUP COMMERCANT");
+  // console.timeEnd("TIME SIGNUP COMMERCANT USERMODEL SAVE");
+  // console.timeEnd("TIME SIGNUP COMMERCANT");
   res.json({ result: true, token: newUserSaved.token });
 });
 
 // POST LOGIN
 router.post("/login", async function (req, res, next) {
-  console.time("TIME LOGIN");
-  console.log(">>req.body", req.body);
+  // console.time("TIME LOGIN");
+  // console.log(">>req.body", req.body);
   let result = false;
   const error = [];
   let token = null;
@@ -132,10 +132,10 @@ router.post("/login", async function (req, res, next) {
     const user = await UserModel.findOne({
       email: req.body.email,
     });
-    console.timeEnd("TIME LOGIN USERMODEL FINDONE");
+    // console.timeEnd("TIME LOGIN USERMODEL FINDONE");
 
     if (user) {
-      console.time("TIME LOGIN BCRYPT COMPARE");
+      // console.time("TIME LOGIN BCRYPT COMPARE");
       if (bcrypt.compareSync(req.body.password, user.password)) {
         //ON COMPARE LE MOT DE PASSE ENVOYE PAR L'UTILISATEUR ET CELUI CRYPTE EN BDD
         result = true;
@@ -144,34 +144,34 @@ router.post("/login", async function (req, res, next) {
         result = false;
         error.push("Email ou mot de passe incorrect");
       }
-      console.time("TIME LOGIN BCRYPT COMPARE");
+      // console.time("TIME LOGIN BCRYPT COMPARE");
     } else {
       error.push("Email ou mot de passe incorrect");
     }
   }
-  console.timeEnd("TIME LOGIN");
+  // console.timeEnd("TIME LOGIN");
   res.json({ result, user, error, token });
 });
 
 //POST SEARCH UTILISATEUR
 router.post("/recherche-utilisateur", async function (req, res, next) {
-  console.time("TIME RECHERCHE UTILISATEUR");
-  console.log("POST SEARCH UTILISATEUR BODY: ", req.body);
+  // console.time("TIME RECHERCHE UTILISATEUR");
+  // console.log("POST SEARCH UTILISATEUR BODY: ", req.body);
   const regexSearch = new RegExp(req.body.search, "i");
-  console.time("TIME RECHERCHE UTILISATEUR QUARTIER MODEL FINDONE");
+  // console.time("TIME RECHERCHE UTILISATEUR QUARTIER MODEL FINDONE");
   const quartierActivity = await QuartierModel.findOne({
     name: req.body.quartierSearch,
   });
-  console.timeEnd("TIME RECHERCHE UTILISATEUR QUARTIER MODEL FINDONE");
+  // console.timeEnd("TIME RECHERCHE UTILISATEUR QUARTIER MODEL FINDONE");
   const quartierId = quartierActivity._id;
-  console.time("TIME RECHERCHE UTILISATEUR USERMODEL FINDONE");
+  // console.time("TIME RECHERCHE UTILISATEUR USERMODEL FINDONE");
   const findUsers = await UserModel.find({
     nameSearch: regexSearch,
     quartierActivity: quartierId,
     status: req.body.typeUtilisateurSearch,
   });
-  console.timeEnd("TIME RECHERCHE UTILISATEUR USERMODEL FINDONE");
-  console.log("findUsers", findUsers);
+  // console.timeEnd("TIME RECHERCHE UTILISATEUR USERMODEL FINDONE");
+  // console.log("findUsers", findUsers);
   const userTableau = [];
   for (let i = 0; i < findUsers.length; i++) {
     const user = findUsers[i];
@@ -184,13 +184,13 @@ router.post("/recherche-utilisateur", async function (req, res, next) {
       status: user.status,
     });
   }
-  console.timeEnd("TIME RECHERCHE UTILISATEUR");
+  // console.timeEnd("TIME RECHERCHE UTILISATEUR");
   res.json({ userTableau });
 });
 
 /*--------------------Add Post-------------------------------*/
 router.post("/addPost", async function (req, res, next) {
-  console.log("req.body", req.body);
+  // console.log("req.body", req.body);
   const searchTokenUser = await UserModel.findOne({
     token: req.body.token,
   });
@@ -204,7 +204,7 @@ router.post("/addPost", async function (req, res, next) {
     name: req.body.quartier,
   });
   const quartierId = searchQuartier._id;
-  console.log("id quartier", quartierId);
+  // console.log("id quartier", quartierId);
 
   const datePost = new Date();
 
@@ -226,20 +226,20 @@ router.post("/addPost", async function (req, res, next) {
 
 // POST RECEPTION ET ENVOI COMMENTAIRE EN BDD
 router.post("/comment", async function (req, res, next) {
-  console.log("récup comment dans route:", req.body);
+  // console.log("récup comment dans route:", req.body);
   const searchUser = await UserModel.findOne({ token: req.body.token });
-  console.log("recherche user via token", searchUser);
+  // console.log("recherche user via token", searchUser);
   const userId = searchUser._id;
-  console.log("userId via token:", userId);
+  // console.log("userId via token:", userId);
   const dateComment = new Date();
-  console.log("date comment:", dateComment);
+  // console.log("date comment:", dateComment);
   const newComment = new CommentaireModel({
     createur: userId,
     content: req.body.comment,
     date: dateComment,
   });
   const newCommentSaved = await newComment.save();
-  console.log("new comment saved:", newCommentSaved);
+  // console.log("new comment saved:", newCommentSaved);
   res.json({ result: true, comment: newCommentSaved });
 });
 // RENVOI POST AU FEED
@@ -252,7 +252,7 @@ router.get("/feed", async function (req, res, next) {
     .populate("createur")
     .populate("quartier")
     .exec();
-  console.log("events", events);
+  // console.log("events", events);
   res.json({ result: true, posts, events });
 });
 
@@ -267,7 +267,7 @@ router.post("/event", async function (req, res, next) {
   const searchQuartier = await QuartierModel.findOne({
     name: req.body.quartier,
   });
-  console.log("Event quartier ID", searchQuartier._id);
+  // console.log("Event quartier ID", searchQuartier._id);
   const quartierId = searchQuartier._id;
   let dateDebutBdd = new Date(req.body.dateDebut);
   let dateFinBdd = new Date(req.body.dateFin);
@@ -286,7 +286,7 @@ router.post("/event", async function (req, res, next) {
 });
 
 router.post("/upload", async function (req, res, next) {
-  console.log("ROUTER POST UPLOAD req.files", req.files);
+  // console.log("ROUTER POST UPLOAD req.files", req.files);
   const adresseTMP = "./tmp/" + uniqid() + ".jpg";
   const resultCopy = await req.files.photo.mv(adresseTMP);
   const resultCloudinary = await cloudinary.uploader.upload(adresseTMP);
@@ -295,13 +295,21 @@ router.post("/upload", async function (req, res, next) {
 });
 
 router.get("/chat", async function (req, res, next) {
-  console.log(">>req.query", req.query);
+  // console.log(">>req.query", req.query);
   const searchUser = await UserModel.findOne({ token: req.query.token });
   const searchMessage = await MessageModel.find({
     emetteur: searchUser._id,
   });
-  console.log("searchMessage", searchMessage);
+  // console.log("searchMessage", searchMessage);
   res.json({ result: true, messages: searchMessage });
+});
+
+router.post("/feed-profil", async function (req, res, next) {
+  const searchUser = await UserModel.findOne({ token: req.body.token });
+  const userId = searchUser._id;
+  const searchUserPost = await PostModel.find({ createur: userId });
+
+  res.json({ result: true, userPosts: searchUserPost, user: searchUser });
 });
 
 module.exports = router;
